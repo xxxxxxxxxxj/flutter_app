297b2425f3ea0c9a2f14f3d720dcd0acb5e95388
xujun <xvjun@haotang365.com.cn>
2019-06-20 11:33:07 +0800

    滑动控件，折叠控件

--- lib/pages/nestedscrollview_page.dart
+++ lib/pages/nestedscrollview_page.dart
@@ -1,7 +1,92 @@
 import 'package:flutter/material.dart';
-class NestedScrollViewPage extends StatelessWidget{
+
+class NestedScrollViewPage extends StatelessWidget {
+  var _tabs = ['TabA', 'TabB'];
+  var colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.pink,
+    Colors.yellow,
+    Colors.deepPurple
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      body: DefaultTabController(
+          length: _tabs.length,
+          child: NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) => <Widget>[
+                    // 传入 handle 值，直接通过 `sliverOverlapAbsorberHandleFor` 获取即可
+                    SliverOverlapAbsorber(
+                      handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                          context),
+                      child: SliverAppBar(
+                        pinned: true,
+                        title: Text('NestedScrollViewPage Demo'),
+                        expandedHeight: 200,
+                        //设置背景
+                        flexibleSpace: FlexibleSpaceBar(
+                          background: Image.asset(
+                            'images/timg.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        bottom: TabBar(
+                            tabs: _tabs
+                                .map((str) => Text(
+                                      str,
+                                      style: TextStyle(fontSize: 18),
+                                    ))
+                                .toList()),
+                        forceElevated: innerBoxIsScrolled,
+                      ),
+                    )
+                  ],
+              body: TabBarView(
+                  // 这边需要通过 Builder 来创建 TabBarView 的内容，否则会报错
+                  // NestedScrollView.sliverOverlapAbsorberHandleFor must be called with a context that contains a NestedScrollView.
+                  children: _tabs
+                      .map((str) => Builder(
+                          builder: (context) => CustomScrollView(
+                                // key 保证唯一性
+                                key: PageStorageKey<String>(str),
+                                slivers: <Widget>[
+                                  // 将子部件同 `SliverAppBar` 重叠部分顶出来，否则会被遮挡
+                                  SliverOverlapInjector(
+                                      handle: NestedScrollView
+                                          .sliverOverlapAbsorberHandleFor(
+                                              context)),
+                                  //gridview
+                                  SliverGrid(
+                                      delegate: SliverChildBuilderDelegate(
+                                          (_, index) =>
+                                              Image.asset('images/ali.jpg'),
+                                          childCount: 33),
+                                      gridDelegate:
+                                          SliverGridDelegateWithFixedCrossAxisCount(
+                                              crossAxisCount: 5,
+                                              mainAxisSpacing: 10,
+                                              crossAxisSpacing: 10,
+                                              childAspectRatio: 1)),
+                                  //listview
+                                  SliverFixedExtentList(
+                                      delegate: SliverChildBuilderDelegate(
+                                          (_, index) => Container(
+                                                alignment: Alignment.center,
+                                                child: Text(
+                                                  '$str - item${index + 1}',
+                                                  style: TextStyle(
+                                                      fontSize: 20,
+                                                      color: colors[index % 6]),
+                                                ),
+                                              ),
+                                          childCount: 10),
+                                      itemExtent: 50)
+                                ],
+                              )))
+                      .toList()))),
+    );
   }
 }
--- lib/pages/sliverpersistentheader_page.dart
+++ lib/pages/sliverpersistentheader_page.dart
@@ -17,9 +17,13 @@ class SliverPersistentHeaderPage extends StatelessWidget {
             delegate: CustomSliverPersistentHeaderDelegate(
                 max: 300,
                 min: 100,
-                child: Text(
-                  '我是一个头部控件',
-                  style: TextStyle(fontSize: 30, color: Colors.white),
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.redAccent,
+                  child: Text(
+                    '我是一个头部控件',
+                    style: TextStyle(fontSize: 20),
+                  ),
                 )),
             pinned: true,
             floating: true,
@@ -40,36 +44,39 @@ class SliverPersistentHeaderPage extends StatelessWidget {
   }
 }
 
+// 自定义 SliverPersistentHeaderDelegate
 class CustomSliverPersistentHeaderDelegate
     extends SliverPersistentHeaderDelegate {
-  final double max; //最大高度
-  final double min; //最小高度
-  final Widget child; //需要展示的内容
+  final double max; // 最大高度
+  final double min; // 最小高度
+  final Widget child; // 需要展示的内容
 
   CustomSliverPersistentHeaderDelegate(
       {@required this.max, @required this.min, @required this.child})
+      // 如果 assert 内部条件不成立，会报错
       : assert(max != null),
         assert(min != null),
         assert(child != null),
         assert(min <= max),
         super();
 
+  // 返回展示的内容，如果内容固定可以直接在这定义，如果需要可扩展，这边通过传入值来定义
   @override
   Widget build(
           BuildContext context, double shrinkOffset, bool overlapsContent) =>
       child;
 
   @override
-  double get maxExtent => max; // 最大高度
+  double get maxExtent => max; // 返回最大高度
 
   @override
-  double get minExtent => min; // 最小高度
+  double get minExtent => min; // 返回最小高度
 
   @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+  bool shouldRebuild(CustomSliverPersistentHeaderDelegate oldDelegate) {
     // 是否需要更新，这里我们定义当高度范围和展示内容被替换的时候进行刷新界面
-    return max != oldDelegate.maxExtent ||
-        min != oldDelegate.minExtent ||
+    return max != oldDelegate.max ||
+        min != oldDelegate.min ||
         child != oldDelegate.child;
   }
 }
